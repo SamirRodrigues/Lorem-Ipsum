90f42ab4d645202c1c1cd0b4f2347bfcc5cc1a8d
SamirRodrigues <emaildesamir@gmail.com>
2021-12-06 00:57:11 -0300

    Updates

---
+++ precache-manifest.c3b4a015422d31af9738112e3a9e5d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f1903b43b47d122963f089ab16e161",
+    "url": "/Lorem-Ipsum/index.html"
+  },
+  {
+    "revision": "00733413c6d459c2ed62",
+    "url": "/Lorem-Ipsum/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/Lorem-Ipsum/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lorem-Ipsum/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00733413c6d459c2ed62",
+    "url": "/Lorem-Ipsum/static/js/main.d6101d2d.chunk.js"
+  },
+  {
+    "revision": "59c51bb8c51425c87209",
+    "url": "/Lorem-Ipsum/static/js/runtime-main.a2a05125.js"
+  }
+]);
